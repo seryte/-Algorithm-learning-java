430d2e1c63601d8156548a73b87cdb5a391725fc
cydia <adm>
2020-10-17 23:47:03 +0800

    first commit

---
+++ alg-demo/src/BubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    //    冒泡排序第一版
+    public static void sort(int array[]) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                int tmp = 0;
+                if (array[j] > array[j + 1]) {
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 8, 6, 3, 9, 2, 1, 7};
+        sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
