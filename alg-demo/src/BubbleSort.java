a519027a0c6bfca7d3dc28c28b38d6b5a660b085
cydia <adm>
2020-10-18 22:41:03 +0800

    冒泡排序优化版

--- alg-demo/src/BubbleSort.java
+++ alg-demo/src/BubbleSort.java
@@ -2,10 +2,10 @@ import java.util.Arrays;
 
 public class BubbleSort {
     //    冒泡排序第一版
-    public static void sort(int array[]) {
+    public static void sort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
-                int tmp = 0;
+                int tmp;
                 if (array[j] > array[j + 1]) {
                     tmp = array[j];
                     array[j] = array[j + 1];
@@ -16,9 +16,100 @@ public class BubbleSort {
         }
     }
 
+    //    冒泡排序第二版，记录顺序，当数组已经有序时，则不需要再排序
+    public static void sort2(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            boolean isSorted = true;
+            for (int j = 0; j < array.length - i - 1; j++) {
+                int tmp;
+                if (array[j] > array[j + 1]) {
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+    //冒泡排序第三版
+    public static void sort3(int[] array) {
+        //记录最后一次交换的位置
+        int lastExchangeIndex = 0;
+        //无序数列的边界，每次比较只需要比到这里为止
+        int sortBorder = array.length - 1;
+
+        for (int i = 0; i < array.length - 1; i++) {
+
+            boolean isSorted = true;
+            for (int j = 0; j < sortBorder; j++) {
+                int tmp;
+                if (array[j] > array[j + 1]) {
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                    // 因为有元素进行交换，所以不是有序的，标记变为false
+                    isSorted = false;
+                    // 更新为最后一次交换元素的位置
+                    lastExchangeIndex = j;
+                }
+            }
+            sortBorder = lastExchangeIndex;
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+
+    //鸡尾酒排序
+    public static void CocktailSort(int[] array) {
+        int tmp;
+        //大循环控制回合，左右各循环一半，完成整个循环
+        for (int i = 0; i < array.length / 2; i++) {
+            boolean isSorted = true;
+            //奇数轮，从左向右比较和交换
+            for (int j = i; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                    isSorted = false;
+
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+
+            // 在偶数轮之前，将isSorted重新标记为true
+            isSorted = true;
+            //偶数轮，从右向左比较和交换
+            for (int j = array.length - i - 1; j > i; j--) {
+                if (array[j] < array[j - 1]) {
+                    tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
         int[] array = new int[]{5, 8, 6, 3, 9, 2, 1, 7};
-        sort(array);
-        System.out.println(Arrays.toString(array));
+        int[] array1 = new int[]{2, 1, 3, 4, 5, 6, 7, 8};
+        int[] array3 = new int[]{2, 3, 4, 5, 6, 7, 8, 1};
+//        sort(array);
+//        System.out.println(Arrays.toString(array));
+
+//        sort3(array1);
+        CocktailSort(array3);
+        System.out.println(Arrays.toString(array3));
     }
 }
